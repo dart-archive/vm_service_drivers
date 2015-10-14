2cf2a5c7d5d9d9b6f88d63f0f2291a045deef901
Devon Carew <devoncarew@gmail.com>
2015-10-14 11:23:49 -0700

    fix java

--- java/src/org/dartlang/vm/service/VmServiceBase.java
+++ java/src/org/dartlang/vm/service/VmServiceBase.java
@@ -1,11 +1,11 @@
 /*
  * Copyright (c) 2015, the Dart project authors.
- * 
+ *
  * Licensed under the Eclipse Public License v1.0 (the "License"); you may not use this file except
  * in compliance with the License. You may obtain a copy of the License at
- * 
+ *
  * http://www.eclipse.org/legal/epl-v10.html
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software distributed under the License
  * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
  * or implied. See the License for the specific language governing permissions and limitations under
@@ -58,7 +58,7 @@ abstract class VmServiceBase implements VmServiceConst {
 
   /**
    * Connect to the VM observatory service via the specified URI
-   * 
+   *
    * @return an API object for interacting with the VM service (not {@code null}).
    */
   public static VmService connect(final String url) throws IOException {
@@ -171,7 +171,7 @@ abstract class VmServiceBase implements VmServiceConst {
 
   /**
    * Connect to the VM observatory service on the given local port.
-   * 
+   *
    * @return an API object for interacting with the VM service (not {@code null}).
    */
   public static VmService localConnect(int port) throws IOException {
@@ -283,7 +283,7 @@ abstract class VmServiceBase implements VmServiceConst {
     });
   }
 
-  public abstract void getObject(String isolateId, String objectId, GetObjectConsumer consumer);
+  public void getObject(String isolateId, String objectId, int offset, int count, GetObjectConsumer consumer);
 
   /**
    * Sends the request and associates the request with the passed {@link Consumer}.
