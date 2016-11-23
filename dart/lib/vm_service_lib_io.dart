88d072e70c35e05a4dc5a3e01d5f40996b305234
Devon Carew <devoncarew@google.com>
2016-11-23 15:41:31 -0800

    remove log statement

--- dart/lib/vm_service_lib_io.dart
+++ dart/lib/vm_service_lib_io.dart
@@ -7,8 +7,17 @@ import 'dart:io';
 
 import 'vm_service_lib.dart';
 
-Future<VmService> vmServiceConnect(String host, int port, {Log log}) async {
+Future<VmService> vmServiceConnect(String host, int port, { Log log }) async {
   WebSocket socket = await WebSocket.connect('ws://$host:$port/ws');
+  StreamController<String> controller = new StreamController();
+  socket.listen((data) => controller.add(data));
+  return new VmService(
+      controller.stream, (String message) => socket.add(message),
+      log: log, disposeHandler: () => socket.close());
+}
+
+Future<VmService> vmServiceConnectUri(String wsUri, { Log log }) async {
+  WebSocket socket = await WebSocket.connect(wsUri);
   StreamController<String> controller = new StreamController();
   socket.listen((data) => controller.add(data));
   return new VmService(
