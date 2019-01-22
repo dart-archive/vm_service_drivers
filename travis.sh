01514d48fe8fcf6c4417aa4c3e97db51b7edf179
Devon Carew <devoncarew@google.com>
2019-01-21 18:59:47 -0800

    refactor the travis bots

---
+++ travis.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+# Copyright (c) 2015, the Dart project authors.  Please see the AUTHORS file
+# for details. All rights reserved. Use of this source code is governed by a
+# BSD-style license that can be found in the LICENSE file.
+
+# Fast fail the script on failures.
+set -e
+
+pushd dart
+
+# Provision pub packages.
+pub get
+
+# Ensure the generator works.
+dart tool/generate.dart
+
+if [ "$BOT" = "dart" ]; then
+
+    # Ensure all the code analyzes cleanly.
+    dartanalyzer --fatal-warnings .
+
+    # Run the VM service protocol smoke tester.
+    dart example/vm_service_lib_tester.dart
+
+elif [ "$BOT" = "java" ]; then
+
+    popd
+    pushd java
+
+    # Run the Java tests.
+    ant -f build.xml
+
+else
+
+    echo "unknown bot configuration"
+    exit 1
+
+fi
+
+popd
