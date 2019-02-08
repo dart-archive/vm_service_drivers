8947d1af0ea9e7348d086141064a8e6a6b1833f3
Devon Carew <devoncarew@google.com>
2019-02-08 08:22:49 -0800

    fix the codegen for getSourceReport (#211)

--- dart/example/sample_main.dart
+++ dart/example/sample_main.dart
@@ -2,6 +2,8 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
+import 'dart:developer' as developer;
+
 void main(List<String> args) {
   String local1 = 'abcd';
   int local2 = 2;
@@ -29,6 +31,9 @@ void main(List<String> args) {
 
   print(longList);
   print(deepList);
+
+  developer.debugger();
+
   print('exiting...');
 }
 
