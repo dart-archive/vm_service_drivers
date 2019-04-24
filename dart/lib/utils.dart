1d01fb8dbebdebeb030648cea4db8a5b010b2450
Devon Carew <devoncarew@google.com>
2019-04-24 08:06:17 -0700

    rename getVmWsUriFromObservatoryUri to convertToWebSocketUrl (#233)

--- dart/lib/utils.dart
+++ dart/lib/utils.dart
@@ -2,17 +2,21 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
-/// Map the URI (which may already be Observatory web app) to a WebSocket URI
-/// for the VM service.
+import 'package:meta/meta.dart';
+
+/// Map the URI to a WebSocket URI for the VM service protocol.
 ///
 /// If the URI is already a VM Service WebSocket URI it will not be modified.
-Uri getVmWsUriFromObservatoryUri(Uri uri) {
-  final isSecure = uri.isScheme('wss') || uri.isScheme('https');
+Uri convertToWebSocketUrl({@required Uri serviceProtocolUrl}) {
+  final isSecure = serviceProtocolUrl.isScheme('wss') ||
+      serviceProtocolUrl.isScheme('https');
   final scheme = isSecure ? 'wss' : 'ws';
 
-  final path = uri.path.endsWith('/ws')
-      ? uri.path
-      : (uri.path.endsWith('/') ? '${uri.path}ws' : '${uri.path}/ws');
+  final path = serviceProtocolUrl.path.endsWith('/ws')
+      ? serviceProtocolUrl.path
+      : (serviceProtocolUrl.path.endsWith('/')
+          ? '${serviceProtocolUrl.path}ws'
+          : '${serviceProtocolUrl.path}/ws');
 
-  return uri.replace(scheme: scheme, path: path);
+  return serviceProtocolUrl.replace(scheme: scheme, path: path);
 }
